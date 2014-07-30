e4baf7978ac488e3528a1932b800f9c69c83aa5c
Daniel Hart <me@kingdingdan.com>
2014-07-30 15:23:33 +0000

    Changes

--- public/js/functions.js
+++ public/js/functions.js
@@ -9,3 +9,16 @@ box3 = $(".box-3").width(boxwidth);
 
 $(document).ready(boxwidth);
 $(window).resize(boxwidth);
+
+//body padding 
+
+var bodypad = function () {
+
+navheight = $(".menu").height();
+bodypadding = $("body").css("padding-top",navheight);
+
+}
+
+$(document).ready(bodypad);
+$(window).resize(bodypad);
+
