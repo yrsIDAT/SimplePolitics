6faae1d243e36ca80a597066aa6de1024c4029cb
Daniel Hart <me@kingdingdan.com>
2014-07-31 15:29:44 +0000

    Styling changes for today

--- public/js/functions.js
+++ public/js/functions.js
@@ -1,24 +1,44 @@
-// Box resize 
-
-var boxwidth = function () {
-
-boxwidth = $(".content").width() / 3 - 5;
-box3 = $(".box-3").width(boxwidth);
-
-}
-
-$(document).ready(boxwidth);
-$(window).resize(boxwidth);
-
 //body padding 
 
 var bodypad = function () {
 
-navheight = $(".menu").height();
+
+if (windowWidth > 700) {
+    navheight = $(".menu").height();
+} else {
+    navheight = $(".mobile-bar").height();
+};
+
 bodypadding = $("body").css("padding-top",navheight);
 
-}
+};
 
+
+
+//Responsive Menu
+
+var nav = function () {
+
+windowWidth = $(window).width();
+
+if (windowWidth > 700) {
+    $(".mobile-menu").css("display", "none");
+    $(".menu").css("display", "block");
+} else {
+    $(".mobile-menu").css("display", "block");
+    $(".menu").css("display", "none");
+}
 $(document).ready(bodypad);
 $(window).resize(bodypad);
+};
+$(document).ready(nav);
+$(window).resize(nav);
 
+
+function toggleMenu() {
+    if ($(".drawer").hasClass('hidden')) {
+        $(".drawer").removeClass('hidden');
+    } else {
+        $(".drawer").addClass('hidden');
+    }
+}
